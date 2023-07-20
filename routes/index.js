4f23689e121d278b23b0e2b17417db17f5f054e6
geon-youn <geon.youn@outlook.com>
2023-07-20 16:54:06 -0400

    add message object and add it to render function

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+const textMessage = (text, user, added) => {
+  return {
+    text,
+    user,
+    added,
+  };
+};
+
+const messages = [
+  textMessage('Hi There!', 'Amando', new Date()),
+  textMessage('Hello World!', 'Charles', new Date()),
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express', messages });
 });
 
 module.exports = router;
